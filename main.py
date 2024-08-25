8698c7c4d045deaf1b86399e27fcb1f4ac2521ba
Ming Liang Khong <khongmingliang@icloud.com>
2024-08-25 17:38:26 +0800

    feat: added rotation to player

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     game_clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill('black')
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
