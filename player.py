1f40a56deda1a0be7ecf55110c8ca25ba956a9ca
Ming Liang Khong <khongmingliang@icloud.com>
2024-08-25 18:15:20 +0800

    feat: added shooting

--- player.py
+++ player.py
@@ -1,7 +1,9 @@
 import pygame
+from pygame import Vector2
 
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 
 
 class Player(CircleShape):
@@ -35,7 +37,15 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
+
