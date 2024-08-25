6333341a36fce1533425899d8d98e8ea7f77dbc8
Ming Liang Khong <khongmingliang@icloud.com>
2024-08-25 17:23:59 +0800

    feat: added game_clock

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
