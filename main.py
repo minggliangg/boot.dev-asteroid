b7e2fbe366ba24e45181ab35b013ff0d632daea9
Ming Liang Khong <khongmingliang@icloud.com>
2024-08-25 17:47:46 +0800

    feat: use groups

--- main.py
+++ main.py
@@ -12,16 +12,25 @@ def main():
     pygame.init()
     game_clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill('black')
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
